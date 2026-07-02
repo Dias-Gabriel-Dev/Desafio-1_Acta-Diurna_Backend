c721e3a19dad7f0667e1be174665e54a7c184c11
Dias-Gabriel-Dev <gabriel.henrique.dias@outlook.com>
2026-07-02 16:48:23 -0300

    Implementação CRUD completa com lógica de validação de dados.

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const createJobSchema = z.object({
   description: z.string().optional()
 });
 
-app.post('/jobs', (req: Request, res: Response) => {
+app.post("/jobs", (req: Request, res: Response) => {
     try {
 
         const validatedData = createJobSchema.parse(req.body);
@@ -41,6 +41,64 @@ app.post('/jobs', (req: Request, res: Response) => {
 });
 
 
-app.get("/jobs", (_req, res) => {
+app.get("/jobs", (req: Request, res: Response) => {
     res.json([]);
+});
+
+app.get("/jobs/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const job = jobs.find((j) => j.id ===id);
+
+    if (!job) {
+        res.status(404).json({ error: 'Vaga não encontrada' });
+        return;
+    }
+
+    res.status(200).json(job);
+});
+
+export const updateJobSchema = createJobSchema.partial();
+
+app.patch("/jobs/:id", (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const jobIndex = jobs.findIndex((j) => j.id === id);
+        const existingJob = jobs[jobIndex];
+
+        if (!existingJob) {
+            res.status(404).json({ error: 'Vaga não encontrada'});
+            return;
+        }
+
+        const validatedData = updateJobSchema.parse(req.body);
+
+        const updateJobs: Job = {
+            ...existingJob,
+            ...validatedData
+        } as Job;
+
+        jobs[jobIndex] = updateJobs;
+        
+        res.status(200).json(updateJobs);
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            res.status(400).json({ errors: error.issues });
+            return;
+        }
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+app.delete("/jobs/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const jobIndex = jobs.findIndex((j) => j.id === id);
+
+    if (jobIndex === -1) {
+        res.status(404).json({error: 'Vaga não encontrada'});
+        return;
+    }
+
+    jobs.splice(jobIndex, 1);
+
+    res.status(204).send();
 });
